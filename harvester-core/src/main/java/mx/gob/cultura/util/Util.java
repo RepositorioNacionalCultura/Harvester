41c53bacd0b7781ce4b55002f695e3ab87ef3e69
juan.fernandez <juan.fernandez@192.168.81.211>
2017-12-21 17:14:51 -0600

    Add loadMetadataPrefixCollection method

--- harvester-core/src/main/java/mx/gob/cultura/util/Util.java
+++ harvester-core/src/main/java/mx/gob/cultura/util/Util.java
@@ -2,20 +2,21 @@ package mx.gob.cultura.util;
 
 import com.mongodb.BasicDBList;
 import com.mongodb.BasicDBObject;
+import com.mongodb.DBCollection;
+import com.mongodb.DBCursor;
+import com.mongodb.DBObject;
 import com.mongodb.MongoClient;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import jdk.nashorn.api.scripting.ScriptObjectMirror;
 import org.apache.http.HttpHost;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestHighLevelClient;
-import org.json.JSONArray;
-import org.json.JSONObject;
 
 import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
 import org.semanticwb.datamanager.DataList;
 import org.semanticwb.datamanager.DataObject;
 import org.semanticwb.datamanager.SWBDataSource;
@@ -27,6 +28,7 @@ import org.semanticwb.datamanager.SWBScriptEngine;
  * @author Hasdai Pacheco
  */
 public final class Util {
+
     private Util() {
     }
 
@@ -34,6 +36,7 @@ public final class Util {
      * Inner class to encapsulate methods related to DataBase actions.
      */
     public static final class DB {
+
         private static HashMap<String, MongoClient> mongoClients = new HashMap<>();
         private static HashMap<String, RestHighLevelClient> elasticClients = new HashMap<>();
 
@@ -59,12 +62,12 @@ public final class Util {
                 ret = new MongoClient(host, port);
                 mongoClients.put(host + ":" + String.valueOf(port), ret);
             }
-
             return ret;
         }
 
         /**
-         * Gets a {@link RestHighLevelClient} instance with default host and port.
+         * Gets a {@link RestHighLevelClient} instance with default host and
+         * port.
          *
          * @return RestHighLevelClient instance object.
          */
@@ -90,7 +93,14 @@ public final class Util {
             return ret;
         }
     }
-     public static BasicDBObject toBasicDBObject(DataObject obj) {
+
+    /**
+     * Transforma un DataObject a BasicDBObject
+     *
+     * @param obj DataObject recibido
+     * @return El BasicJSONObject traducido desde un DataObject
+     */
+    public static BasicDBObject toBasicDBObject(DataObject obj) {
         BasicDBObject ret = new BasicDBObject();
         Iterator<Map.Entry<String, Object>> it = obj.entrySet().iterator();
         while (it.hasNext()) {
@@ -100,6 +110,11 @@ public final class Util {
         return ret;
     }
 
+    /**
+     * 
+     * @param obj
+     * @return
+     */
     public static Object toBasicDB(Object obj) {
         if (obj instanceof DataObject) {
             return toBasicDBObject((DataObject) obj);
@@ -109,6 +124,11 @@ public final class Util {
         return obj;
     }
 
+    /**
+     * Traduce un DataList a BasicDBList
+     * @param obj
+     * @return
+     */
     public static BasicDBList toBasicDBList(DataList obj) {
         BasicDBList ret = new BasicDBList();
         Iterator it = obj.iterator();
@@ -117,21 +137,19 @@ public final class Util {
         }
         return ret;
     }
-    
-    
-    
-    
+
     /**
-     * Carga la colección de Replace a un HashMap<ocurrencia, reemplazo> 
+     * Carga la colección de Replace a un HashMap<ocurrencia, reemplazo>
      *
-     * @param engine Utilizado para poder cargar la colección de Replace en un HashMap
+     * @param engine Utilizado para poder cargar la colección de Replace en un
+     * HashMap
      * @return HashMap con DataSource cargado en memoria.
      */
-    public static HashMap<String,String> loadOccurrences( SWBScriptEngine engine) {
+    public static HashMap<String, String> loadOccurrences(SWBScriptEngine engine) {
 
         SWBDataSource datasource = null;
-        HashMap<String,String> hm = new HashMap();
-        
+        HashMap<String, String> hm = new HashMap();
+
         if (null != engine) {
             try {
                 datasource = engine.getDataSource("Replace");
@@ -164,9 +182,49 @@ public final class Util {
 
         return hm;
     }
-    
-    
-    
+
+    /**
+     * Carga la colección de Prefijo de metadata a un
+     * ConcurrentHashMap<identificador, DataObject>
+     *
+     * @param dbName Nombre de la base de datos en donde se encuentra la
+     * colección
+     * @param metadataPrefix Nombre del prefijo de metadatos a cargar
+     * @return ConcurrentHashMap con la colección cargada en memoria.
+     */
+    public static ConcurrentHashMap<String, DataObject> loadMetadataPrefixCollection(String dbName, String metadataPrefix) {
+
+        ConcurrentHashMap<String, DataObject> hm = new ConcurrentHashMap();
+        MongoClient client = new MongoClient("localhost", 27017);
+        com.mongodb.DB db = client.getDB(dbName.toUpperCase());
+
+        if (null != db) {
+            try {
+                DBCollection datasource = db.getCollection(metadataPrefix);
+                DataObject r = new DataObject();
+                DataObject data = new DataObject();
+                r.put("data", data);
+
+                DBCursor cursor = datasource.find();
+                while (cursor.hasNext()) {
+                    DBObject next = cursor.next();
+                    String key = (String) next.get("oaiid");
+                    DataObject dataO = new DataObject();
+                    dataO.put("oaiid", key);
+                    dataO.put("body", next.get("body"));
+                    hm.put(key, dataO);
+                }
+            } catch (Exception e) {
+                System.out.println("Error al cargar el DataSource. " + e.getMessage());
+                e.printStackTrace(System.out);
+            }
+        } else {
+            System.out.println("Error al cargar el DataSource al HashMap, falta inicializar el engine.");
+            return null;
+        }
+        return hm;
+    }
+
     /**
      * Reemplaza las ocurrencias en el string recibido
      *
@@ -174,21 +232,29 @@ public final class Util {
      * @param oaistr Stream del registro OAI a revisar
      * @return String con todas las ocurrencias reemplazadas.
      */
-    public static String replaceOccurrences(HashMap<String,String> hm, String oaistr) {
-        if (null != hm && null!=oaistr) {
-                String occurrence = "";
-                String replace = "";
-                Iterator<String> it = hm.keySet().iterator();
-                while (it.hasNext()) {
-                    occurrence = it.next();
-                    replace = hm.get(occurrence);
-                     oaistr = oaistr.replace(occurrence, replace);
-                }
-        } 
+    public static String replaceOccurrences(HashMap<String, String> hm, String oaistr) {
+        if (null != hm && null != oaistr) {
+            String occurrence = "";
+            String replace = "";
+            Iterator<String> it = hm.keySet().iterator();
+            while (it.hasNext()) {
+                occurrence = it.next();
+                replace = hm.get(occurrence);
+                oaistr = oaistr.replace(occurrence, replace);
+            }
+        }
         return oaistr;
     }
-    
-    public static String makeRequest(URL theUrl, boolean XMLSupport)  {
+
+    /**
+     * Make the URL Request from the extractor
+     *
+     * @param theUrl La Url de la cual se extraeran los datos
+     * @param XMLSupport Si soporta XML la llamada, o lo trae directamente en
+     * JSON
+     * @return Los datos traidos de la Url recibida
+     */
+    public static String makeRequest(URL theUrl, boolean XMLSupport) {
         HttpURLConnection con = null;
         StringBuilder response = new StringBuilder();
         String errorMsg = null;
@@ -203,44 +269,42 @@ public final class Util {
                     con.setRequestProperty("accept", "application/xml");
                 }
                 con.setRequestMethod("GET");
-                //System.out.println("content type:" + con.getContentType());
 
-                
-                    int statusCode = con.getResponseCode();
+                int statusCode = con.getResponseCode();
 
-                    if (statusCode == 200) {
-                        try (BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()))) {
-                            String inputLine;
+                if (statusCode == 200) {
+                    try (BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()))) {
+                        String inputLine;
 
-                            while ((inputLine = in.readLine()) != null) {
-                                response.append(inputLine);
-                            }
-                        } catch (IOException ioex) {
-                            ioex.printStackTrace();
+                        while ((inputLine = in.readLine()) != null) {
+                            response.append(inputLine);
                         }
-                    } else {
-                        //throw new ServerErrorException();
+                    } catch (IOException ioex) {
+                        ioex.printStackTrace();
                     }
-                
+                } else {
+                    //throw new ServerErrorException();
+                }
+
             } catch (IOException e) {
                 if (null != con) {
                     con.disconnect();
                 }
                 retries++;
                 try {
-                     Thread.sleep(5000);
+                    Thread.sleep(5000);
                 } catch (Exception te) {
                     te.printStackTrace();
                 }
-               
+
                 e.printStackTrace();
                 isConnOk = false;
-                if(retries==5){
-                    errorMsg="#Error: No se puede conectar al servidor#";
+                if (retries == 5) {
+                    errorMsg = "#Error: No se puede conectar al servidor#";
                 }
             }
-        } while (isConnOk == false && retries < 5); 
-        return errorMsg!=null?errorMsg:response.toString();
+        } while (isConnOk == false && retries < 5);
+        return errorMsg != null ? errorMsg : response.toString();
     }
 
 }
